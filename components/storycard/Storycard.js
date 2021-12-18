bc7d859a8f8bfefaecc4031af58599e4509c1a53
Ye <ye914617@gmail.com>
2021-12-18 15:39:46 +0800

    Wrap story card Image with parent position relative and w-100 h-100

--- components/storycard/Storycard.js
+++ components/storycard/Storycard.js
@@ -5,10 +5,17 @@ const Storycard = ({ src, link, name, description }) => {
   return (
     <div className="mx-1 sm:w-screen/12 sm:h-screen/3 relative hover:scale-105 cursor-pointer">
       <div className="w-8 h-8 sm:w-11 sm:h-11 sm:absolute border-4 border-blue-700 rounded-full sm:top-2 sm:left-2 sm:z-10">
-        <Image src={src} layout="fill" className="rounded-full" />
+        <div className="relative w-full h-full">
+          <Image src={src} layout="fill" className="rounded-full" />
+        </div>
       </div>
-      <div className="opacity-90 hidden sm:block">
-        <Image src={link} layout="fill" className="object-cover rounded-xl" />
+      <div className="opacity-90 hidden sm:block w-full h-full relative">
+        <Image
+          src={link}
+          layout="fill"
+          className="object-cover rounded-xl"
+          priority="true"
+        />
       </div>
     </div>
   );
